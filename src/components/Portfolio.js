90c7e8f786773ef43dd7dec522daa8af64092f7a
Hoang-Minh <quataoxanh83@gmail.com>
2020-07-22 16:33:54 -0700

    update projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import {
-  Box,
-  Grid,
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  CardActions,
-  Button,
-  Typography,
-} from "@material-ui/core";
+import { Box, Grid } from "@material-ui/core";
 import Navbar from "./Navbar";
-import project1 from "../images/html-css-javascript.jpg";
-import project2 from "../images/javascript-fullstack.jpg";
+import Project from "./Project";
+import movieApp from "../images/movieApp.png";
+import emailMe from "../images/emailMe.png";
+import covid19 from "../images/covid19.png";
+import chatApp from "../images/chatApp.png";
+import adviceApp from "../images/adviceApp.png";
+import weatherApp from "../images/weatherApp.png";
+import youtubeApp from "../images/youtubeApp.png";
 
 const useStyles = makeStyles({
   mainContainer: {
@@ -28,141 +24,86 @@ const useStyles = makeStyles({
 
 function Portfolio() {
   const classes = useStyles();
+
   return (
     <Box component="div" className={classes.mainContainer}>
       <Navbar></Navbar>
       <Grid container justify="center">
-        {/* Project 1 */}
-        <Grid item xs={12} sm={8} md={6}>
-          <Card className={classes.cardContainer}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Project 1"
-                height="140"
-                image={project1}
-              ></CardMedia>
-              <CardContent>
-                <Typography gutterBottom variant="h5">
-                  Project 1
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
-                  architecto quo labore, amet, maxime illum exercitationem velit
-                  id facere placeat veritatis! Iusto architecto sunt possimus
-                  illum aliquam itaque, veniam sapiente.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Live Demo
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
+        <Project
+          alt="Movie App"
+          image={movieApp}
+          title="Movie App"
+          link="https://agile-forest-75094.herokuapp.com/"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+          architecto quo labore, amet, maxime illum exercitationem velit
+          id facere placeat veritatis! Iusto architecto sunt possimus
+          illum aliquam itaque, veniam sapiente."
+        ></Project>
+        <Project
+          alt="Survey Email App"
+          image={emailMe}
+          title="Survey App"
+          link="https://cryptic-sea-54086.herokuapp.com/"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+          architecto quo labore, amet, maxime illum exercitationem velit
+          id facere placeat veritatis! Iusto architecto sunt possimus
+          illum aliquam itaque, veniam sapiente."
+        ></Project>
 
-        {/* Project 2 */}
-        <Grid item xs={12} sm={8} md={6}>
-          <Card className={classes.cardContainer}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Project 2"
-                height="140"
-                image={project2}
-              ></CardMedia>
-              <CardContent>
-                <Typography gutterBottom variant="h5">
-                  Project 2
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
-                  architecto quo labore, amet, maxime illum exercitationem velit
-                  id facere placeat veritatis! Iusto architecto sunt possimus
-                  illum aliquam itaque, veniam sapiente.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Live Demo
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
+        <Project
+          alt="Covid 19 Stats"
+          image={covid19}
+          title="Covid 19 Stats"
+          link="https://nameless-taiga-47211.herokuapp.com/"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+          architecto quo labore, amet, maxime illum exercitationem velit
+          id facere placeat veritatis! Iusto architecto sunt possimus
+          illum aliquam itaque, veniam sapiente."
+        ></Project>
 
-        {/* Project 1 */}
-        <Grid item xs={12} sm={8} md={6}>
-          <Card className={classes.cardContainer}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Project 1"
-                height="140"
-                image={project1}
-              ></CardMedia>
-              <CardContent>
-                <Typography gutterBottom variant="h5">
-                  Project 3
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
-                  architecto quo labore, amet, maxime illum exercitationem velit
-                  id facere placeat veritatis! Iusto architecto sunt possimus
-                  illum aliquam itaque, veniam sapiente.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Live Demo
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
+        <Project
+          alt="Chat App"
+          image={chatApp}
+          title="Chat App"
+          link="https://nameless-taiga-47211.herokuapp.com/"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+          architecto quo labore, amet, maxime illum exercitationem velit
+          id facere placeat veritatis! Iusto architecto sunt possimus
+          illum aliquam itaque, veniam sapiente."
+        ></Project>
 
-        {/* Project 1 */}
-        <Grid item xs={12} sm={8} md={6}>
-          <Card className={classes.cardContainer}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Project 1"
-                height="140"
-                image={project1}
-              ></CardMedia>
-              <CardContent>
-                <Typography gutterBottom variant="h5">
-                  Project 4
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
-                  architecto quo labore, amet, maxime illum exercitationem velit
-                  id facere placeat veritatis! Iusto architecto sunt possimus
-                  illum aliquam itaque, veniam sapiente.
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Live Demo
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
+        <Project
+          alt="Simple Advice App"
+          image={adviceApp}
+          title="Simple Advice App"
+          link="https://hopeful-benz-d6952b.netlify.app/"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+          architecto quo labore, amet, maxime illum exercitationem velit
+          id facere placeat veritatis! Iusto architecto sunt possimus
+          illum aliquam itaque, veniam sapiente."
+        ></Project>
+
+        <Project
+          alt="PWA Weather App"
+          image={weatherApp}
+          title="PWA Weather App"
+          link="https://quizzical-galileo-95cd9d.netlify.app/"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+          architecto quo labore, amet, maxime illum exercitationem velit
+          id facere placeat veritatis! Iusto architecto sunt possimus
+          illum aliquam itaque, veniam sapiente."
+        ></Project>
+
+        <Project
+          alt="Simple Youtube Cloner"
+          image={youtubeApp}
+          title="Simple Youtube Cloner"
+          link="https://unruffled-gates-5c1209.netlify.app/"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+          architecto quo labore, amet, maxime illum exercitationem velit
+          id facere placeat veritatis! Iusto architecto sunt possimus
+          illum aliquam itaque, veniam sapiente."
+        ></Project>
       </Grid>
     </Box>
   );
---
+++ src/components/Project.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Button,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  mainContainer: {
+    background: "#233",
+    height: "100%",
+  },
+  cardContainer: {
+    maxWidth: 345,
+    margin: "5rem auto",
+  },
+});
+
+function Project({ alt, image, title, content, link }) {
+  const classes = useStyles();
+
+  return (
+    <Grid item xs={12} sm={8} md={6}>
+      <Card className={classes.cardContainer}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt={alt}
+            height="140"
+            image={image}
+          ></CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {content}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button
+            size="small"
+            color="primary"
+            onClick={() => (window.location.href = `${link}`)}
+          >
+            Live Demo
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
+}
+
+//https://agile-forest-75094.herokuapp.com/
+export default Project;
