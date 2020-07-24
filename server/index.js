66ef30114d933eec6460e3d7e296400a7da85a82
Hoang-Minh <quataoxanh83@gmail.com>
2020-07-23 22:50:53 -0700

    update files for heroku deployment

--- server/index.js
+++ server/index.js
@@ -8,10 +8,17 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/api", (req, res) => {
-  console.log("hello");
-  res.json({ hi: "there" });
-});
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file or main.css file
+  app.use(express.static("build"));
+
+  // Express will serve up the index.html file if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
 
 app.post("/api/send_feedback", async (req, res) => {
   console.log(req.body);
